c993663d4288c9b2d2afce8b42075f62869d4199
Stéphane <stephane.baccam@orencash.fr>
2020-12-18 14:10:00 +0100

    Formulaire d'inscription

--- src/Form/MagasinType.php
+++ src/Form/MagasinType.php
@@ -13,6 +13,7 @@ class MagasinType extends AbstractType
     {
         $builder
             ->add('nom')
+            //->add('stocks')
         ;
     }
 
--- src/Form/UtilisateurType.php
+++ src/Form/UtilisateurType.php
@@ -13,7 +13,6 @@ class UtilisateurType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
             ->add('password')
         ;
     }
